52ca98164871566d71d1ca24417f2dba622a11db
idee1140 <idee1140@gmail.com>
2021-05-21 12:13:09 +0100

    initial commit

---
+++ app/src/main/java/com/example/pointzi_challange/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pointzi_challange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
