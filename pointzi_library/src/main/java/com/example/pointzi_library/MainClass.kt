bd1eb35ea23a3af8d34271ac344d4ce5ce6d41f3
idee1140 <idee1140@gmail.com>
2021-05-21 12:27:11 +0100

    test drive

---
+++ pointzi_library/src/main/java/com/example/pointzi_library/MainClass.kt
@@ -0,0 +1,15 @@
+package com.example.pointzi_library
+
+import android.util.Log
+
+/**
+ *@Created by Yerimah on 5/21/2021.
+ */
+
+class MainClass {
+    
+    private fun logPresence() {
+        println("Presence")
+    }
+
+}
